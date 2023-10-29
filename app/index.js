8f7db501eec3bc637191fcee96a0064716c63ea1
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-10-29 08:03:40 +0700

    create template for routes

--- app/index.js
+++ app/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 const PORT = 3030;
 const HOST = '0.0.0.0';
 
 app.use(express.json());
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/*', (req, res) => {
+  res.status(404).send('Not Found');
 });
 
 app.listen(PORT, HOST, () => {
