e4258d8178133cda7c6417fb1259045f2f0b4bcb
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-10-29 14:40:05 +0700

    create custom application error

---
+++ app/errors/InternalServerError.js
@@ -0,0 +1,9 @@
+class InternalServerError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'InternalServerError';
+    this.statusCode = 500;
+  }
+}
+
+module.exports = InternalServerError;
